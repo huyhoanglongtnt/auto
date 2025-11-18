6da9e0d95c12af092efde0836ec29f585efed9cc
huy <huyhoanglongtnt@gmail.com>
2025-11-18 21:29:21 +0700

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,7 +50,7 @@
             <!-- Sản phẩm mới -->
             <div class="row">
                 <div class="col-12 text-center mb-4">
-                    <h4>SẢN PHẨM MỚI</h4>
+                    <h4 class="fs-1">SẢN PHẨM MỚI</h4>
                 </div>
             </div> 
             <div class="row">
@@ -71,7 +71,15 @@
                         <div class="car__item__text">
                             <div class="car__item__text__inner"> 
                                 <h5><a href="{{ route('products.show', $product) }}">{{ $product->name }}</a></h5> 
-                                <p class="card-text">Mã sản phẩm: {{ $product->sku }}</p>
+                                @ìf($product->brand)
+                                    <p class="card-text">Thương hiệu: {{ $product->brand->name }}</p>
+                                @endif
+                                @if($product->category)
+                                    <p class="card-text">Danh mục: {{ $product->category->name }}</p>
+                                @endif
+                                @if($product->sku)
+                                    <p class="card-text">Mã sản phẩm: {{ $product->sku }}</p>
+                                @endif
                                 <p class="card-text">Giá: {{ number_format($product->price, 0, '.', ',') }} VNĐ</p>
                                 <div class="car__item__prưice mt-2 mb-4"> 
                                     <a href="{{ route('products.show', $product) }}" class="btn btn-info btn-sm">Chi tiết</a>
